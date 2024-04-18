3e52dab9abf197062a26ac2b57142ff9bcc1dab7
Andreas Karis <ak.karis@gmail.com>
2024-04-18 14:18:22 +0200

    next
    Signed-off-by: Andreas Karis <ak.karis@gmail.com>

--- blue.sh
+++ blue.sh
@@ -13,4 +13,12 @@ ip link add tap0 type gretap local "${LOCAL_IP}" remote "${REMOTE_IP}" ikey 0.0.
 ip link set dev tap0 up
 ip a a dev tap0 2000::1/64
 
+FEATURES="tcp-segmentation-offload generic-segmentation-offload generic-receive-offload large-receive-offload tx-checksumming scatter-gather rx-vlan-offload highdma rx-vlan-filter tx-gre-segmentation tx-gre-csum-segmentation tx-ipxip4-segmentation tx-ipxip6-segmentation tx-udp_tnl-segmentation tx-udp_tnl-csum-segmentation tx-esp-segmentation tx-udp-segmentation esp-hw-offload esp-tx-csum-hw-offload tx-sctp-segmentation tx-gso-list rx-checksumming receive-hashing tx-gso-partial rx-all rx-checksumming receive-hashing tx-gso-partial rx-all tx-vlan-stag-hw-insert tx rx rx-udp-gro-forwarding rx-vlan-stag-hw-parse rx-gro-hw scatter-gather"
+INTERFACES=$(ip --json link | jq -r '.[].ifname')
+for interface in ${INTERFACES}; do
+  for feature in ${FEATURES}; do
+    ethtool -K "${interface}" "${feature}" ${SWITCH_ON_OFF}
+  done
+done
+
 sleep infinity
